207d6af079adf51d702d43dd5561911aee519b0a
Luke de Oliveira <lukepercivaldeoliveira@hotmail.com>
2014-02-07 16:59:45 -0500

    got some basic unit tests started

--- include/parser.hh
+++ include/parser.hh
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 namespace optionparser
 {
@@ -24,39 +25,36 @@ enum storage_mode { store_true, store_value, store_mult_values };
 class parser
 {
 public:
-	parser();
-	~parser();
+	parser() = default;
+	~parser()= default;
 
 	// void eat_arguments(int argc, char const *argv[]);
 
-	void add_option(std::string longoption, std::string shortoption, storage_mode mode, std::string key = "", bool required = false);
-	void add_option(std::string shortoption, storage_mode mode, std::string key = "", bool required = false);
+	void add_option(std::string longoption, std::string shortoption, storage_mode mode, bool required = false, std::string key = "");
+	// void add_option(std::string shortoption, storage_mode mode, std::string key = "", bool required = false);
 
 	// template <class T>
 	// T get_value(std::string key);
 
-private:
+// private:
 	Archive m_options;
 	Dictionary m_long_flags, m_short_flags;
 };
 
 void parser::add_option(std::string longoption, std::string shortoption, 
-	storage_mode mode, std::string key = "", bool required = false)
+	storage_mode mode, bool required, std::string key)
 {
 	if (key == "")
 	{
 		key = remove_character(longoption, '-');
 	}
+	std::string req_string = "optional";
 	if (required)
 	{
-		std::string req_string = "required";
+		req_string = "required";
 	}
-	else
-	{
-		std::string req_string = "optional";
-	}
-	m_long_flags[req_string][key] = shortoption;
-	m_short_flags[req_string][key] = longoption;
+	m_long_flags[req_string][key] = longoption;
+	m_short_flags[req_string][key] = shortoption;
 }
 
 
@@ -69,7 +67,7 @@ void parser::add_option(std::string longoption, std::string shortoption,
 
 
 
-inline std::string remove_character(std::string str, char c)
+inline std::string remove_character(std::string str, const char c)
 {
     std::string::iterator end_pos = std::remove(str.begin(), str.end(), c);
     str.erase(end_pos, str.end());
---
+++ unittest.cxx
@@ -0,0 +1,37 @@
+#include "include/parser.hh"
+#include <iostream>
+
+// class unittest
+// {
+// public:
+// 	unittest();
+
+// 	~unittest();
+// };
+
+#include <fstream>
+
+
+template <class T, class U>
+std::ostream& operator << ( std::ostream& o, const std::map<T, U> &m)
+{
+    for (auto &e : m)
+    {
+        o << e.first << ":\n" << e.second << std::endl;
+    }
+    return o;
+}
+
+int main(int argc, char const *argv[])
+{
+	optionparser::parser p;
+
+	p.add_option("--help", "-h", optionparser::store_true);
+	p.add_option("--file", "-f", optionparser::store_true, 1);
+
+	std::cout << p.m_long_flags << std::endl;
+	std::cout << p.m_short_flags << std::endl;
+
+
+	return 0;
+}
